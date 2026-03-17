5fa13f676f937e0785fa435f71851f648153d3b3
BeycanDeveloper <halilbeycan0@gmail.com>
2026-03-18 02:22:05 +0700

    agentic endpoints

--- server/routes/public/index.ts
+++ server/routes/public/index.ts
@@ -6,6 +6,7 @@ import Tasks from "./tasks";
 import Proposals from "./proposals";
 import UserRoutes from "./user";
 import Swap from "./swap";
+import Agentic from "./agentic";
 
 const router = Router();
 
@@ -14,5 +15,6 @@ router.use("/tasks", Tasks);
 router.use("/proposals", Proposals);
 router.use("/user", UserRoutes);
 router.use("/swap", Swap);
+router.use("/agentic", Agentic);
 
 export default router;
